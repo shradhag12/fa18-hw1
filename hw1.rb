21a491180bcaa898043df3222767970f5e9ae47c
shradhag12 <43488463+shradhag12@users.noreply.github.com>
2018-09-24 16:27:20 -0700

    Update hw1.rb

--- hw1.rb
+++ hw1.rb
@@ -1,15 +1,20 @@
 def squared_sum(a, b)
-  # Q1 CODE HERE
+  return ((a + b) ** 2)
 
 end
 
 def sort_array_plus_one(a)
-  # Q2 CODE HERE
+  a.sort
+  i = 0 
+  while i < a.length: 
+    a[i] += 1
+    i += 1
+   return a 
 
 end
 
 def combine_name(first_name, last_name)
-  # Q3 CODE HERE
+ 
 
 end
 
